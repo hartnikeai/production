485fa87d71811afbdb5d9bdacfb7f2ebdd0e65e7
hartnikeai <166290735+hartnikeai@users.noreply.github.com>
2024-04-07 11:18:17 +0200

    Add files via upload

---
+++ DebenuPDFLibrary.pas
@@ -0,0 +1,17 @@
+unit DebenuPDFLibrary;
+
+interface
+
+uses
+  DebenuPDFLibrary1311;
+
+const
+  LibVer: AnsiString = '13.11';
+  LibVerNum = 13.11;
+
+type
+  TDebenuPDFLibrary = class(TDebenuPDFLibrary1311);
+
+implementation
+
+end.
